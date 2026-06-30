02961d00c2ab4597d59eee3fa906954502e8b9d7
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-30 21:20:59 +0530

    move student data to a seperate file

---
+++ data/student.js
@@ -0,0 +1,22 @@
+const students = [
+    {
+        id: 1,
+        name: "Divanshu",
+        course: "B.Tech CSE",
+        age: 18
+    },
+    {
+        id: 2,
+        name: "Rahul",
+        course: "B.Tech IT",
+        age: 19
+    },
+    {
+        id: 3,
+        name: "Aman",
+        course: "BCA",
+        age: 20
+    }
+];
+
+module.exports = students;
--- server.js
+++ server.js
@@ -6,26 +6,7 @@ app.use(express.json());
 
 const PORT = 3000;
 
-const students = [
-    {
-        id: 1,
-        name: "Divanshu",
-        course: "B.Tech CSE",
-        age: 18
-    },
-    {
-        id: 2,
-        name: "Rahul",
-        course: "B.Tech IT",
-        age: 19
-    },
-    {
-        id: 3,
-        name: "Aman",
-        course: "BCA",
-        age: 20
-    }
-];
+const students = require("./data/students");
 
 app.get("/", (req, res) => {
     res.send("<h1>🚀 Welcome to Student Management API</h1>");
