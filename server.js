e13c6a81ccf671c8c87e45d28561ddffb1b63819
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-30 20:54:29 +0530

    add basic routes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get("/", (req, res) => {
     res.send("<h1>🚀 Welcome to Student Management API</h1>");
 });
 
+app.get("/about", (req, res) => {
+    res.send("<h2>About Student Management API</h2>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h2>Contact: support@studentapi.com</h2>");
+});
+
+app.get("/health", (req, res) => {
+    res.send("API is running successfully ✅");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
